2b280f5c3bb83cb718479ca809f7620046451df7
Алина Кударенко <alina@MacBook-Pro-Andrej.local>
2023-04-14 02:53:55 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://6d98956d-38fb-4326-bd47-909c8c5c4709.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
